4a215f1120bd3268b886a8123d5a135e9a7f7ce3
JUZKEN <mferro.acc@gmail.com>
2021-02-07 19:50:37 +0000

    Changed primary colors + styling

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -7,7 +7,13 @@ class Dashboard extends Component {
             <div className="row">
                <div className="col-md-8">
                   {/* TODO: Update username on greetings title */}
-                  <h1 className="Dashboard-greetings">Hi Juzken!</h1>
+                  <h1 className="Dashboard-greetings Dashboard-box">Hi Juzken!</h1>
+                  <div className="Dashboard-earnings Dashboard-box">
+                     <h2>Earnings</h2>
+                  </div>
+                  <div className="Dashboard-activity Dashboard-box">
+                     <h2>Activity</h2>
+                  </div>
                </div>
                <div className="col-md-4">
                   Listings go here...
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,8 +5,6 @@ class Header extends Component {
    render() {
       return (
          <section id="Header">
-            {/* TODO: Make page title dynamic */}
-            <h2 className="Page-title">Dashboard</h2>
             <ActionButtons />
          </section>
       );
