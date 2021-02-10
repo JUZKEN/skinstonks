e8424486871eb20ab500ce000e582eac12103c85
JUZKEN <mferro.acc@gmail.com>
2021-02-10 16:51:15 +0000

    Heroku postbuild client

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const config = require('config');
 const express = require('express');
 const mongoose = require('mongoose');
 const winston = require('winston');
+const path = require('path');
 const app = express();
 
 require('./logging')();
@@ -17,6 +18,15 @@ mongoose.connect(database, {
 
 // View engine
 app.set('view engine', 'pug');
+
+// This middleware informs the express application to serve our compiled React files
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+   app.use(express.static(path.join(__dirname, 'client/build')));
+
+   app.get('*', function (req, res) {
+       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+   });
+};
 
 // Routes
 require('./routes')(app);
