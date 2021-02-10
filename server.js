3f43482e8368f7816b9994b757fdda5270527efe
JUZKEN <mferro.acc@gmail.com>
2021-02-10 20:25:40 +0000

    Fix api endpoints

--- server.js
+++ server.js
@@ -22,10 +22,6 @@ app.set('view engine', 'pug');
 // This middleware informs the express application to serve our compiled React files
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
    app.use(express.static(path.join(__dirname, 'client/build')));
-
-   app.get('*', function (req, res) {
-       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-   });
 };
 
 // Routes
