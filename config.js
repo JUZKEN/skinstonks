1dd063983a46c394e0f42f2f569d39af5f5ae8ee
JUZKEN <mferro.acc@gmail.com>
2021-02-10 20:31:11 +0000

    Check for bitskins api environment vars

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 const config = require('config');
 
 module.exports = function() {
+   if (!config.get('BITSKINS_API_KEY')) throw new Error('FATAL ERROR: skinstonks_BITSKINS_API_KEY is not defined.');
+   if (!config.get('BITSKINS_SECRET')) throw new Error('FATAL ERROR: skinstonks_BITSKINS_SECRET is not defined.');
    if (!config.get('SENDGRID.API_KEY')) throw new Error('FATAL ERROR: SENDGRID_API_KEY is not defined.');
    if (!config.get('SENDGRID.USERNAME')) throw new Error('FATAL ERROR: SENDGRID_USERNAME is not defined.');
    if (!config.get('SENDGRID.PASSWORD')) throw new Error('FATAL ERROR: SENDGRID_PASSWORD is not defined.');
    if (!config.get('SENDGRID.SENDER')) throw new Error('FATAL ERROR: SENDGRID_SENDER is not defined.');
-   if (!config.get('JWT_SECRET')) throw new Error('FATAL ERROR: JWT_SECRET is not defined.');
+   if (!config.get('JWT_SECRET')) throw new Error('FATAL ERROR: skinstonks_JWT_SECRET is not defined.');
 }
