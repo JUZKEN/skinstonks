9b377dda51a9432b26ff8f86154d8716d193f9ba
JUZKEN <mferro.acc@gmail.com>
2021-03-24 19:55:45 +0000

    Removed routes to buy/sell items on bitskins

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,12 +5,10 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../controllers/user');
-const Bitskins = require('../controllers/bitskins');
 
 router.get('/', [auth, admin], User.index);
 router.get('/me', auth, User.me);
 router.put('/me', auth, validate.userUpdate, User.update);
-router.post('/update_api_keys', auth, validate.set_api_keys, Bitskins.update);
 router.delete('/:id', [auth, admin], User.delete);
 
 module.exports = router;
